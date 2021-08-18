709e70a7d7d021f3670abef4698800b73e6d93f3
Suraj Mourya <suraj.mourya@blrsimplilearn.com>
2021-08-18 13:07:13 +1100

    3rd commit Added CSS to the todo app

--- src/components/AddTodoList.js
+++ src/components/AddTodoList.js
@@ -1,13 +1,14 @@
 import {Component} from "react";
+import "./AddTodoList.css"
 
 export default class AddTodoList extends Component {
   render()
   {
       const handleSubmit= event =>{ event.preventDefault();}
     return (
-      <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Enter the todo task"></input>
-          <button>Add</button>
+      <form className ="todo-form"onSubmit={handleSubmit}>
+          <input type="text" placeholder="Enter the task here"></input>
+          <button className="btn blue-btn">Add</button>
       </form>
     );
   }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,15 @@
 import {Component} from "react";
+import "./AddTodoList.css"
+
 
 export default class Todo extends Component {
 
   render(){
    return (
-      <div >
+      <div class="todo-item">
           <p>{this.props.description}</p>
-          <input type="checkbox"></input>
-          <button>Delete</button>
+          <button className="btn red">Done</button>
+          <button className="btn red-btn">Delete</button>
       </div>
     );
   }
